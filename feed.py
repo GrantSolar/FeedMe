1d9eba8f8e971708225fe0821fa233b9933a36b9
Oliver Davey <o.p.davey@gmail.com>
2013-07-26 16:21:58 +0100

    text and working thumbnails from rss

--- feed.py
+++ feed.py
@@ -5,6 +5,26 @@ import urllib
 from PIL import Image
 from io import BytesIO
 
+
+#Returns the image at url as a pygame surface
+def get_image(url):
+    
+    #Open image from url
+    img_file = urllib.request.urlopen(url)
+    img = BytesIO(img_file.read())
+    img = Image.open(img)
+
+    mode = img.mode
+    size = img.size
+    data = img.tostring()
+
+    assert mode in "RGB", "RGBA"
+
+    #Convert image url to pygame surface
+    surface = pygame.image.fromstring(data, size, mode)
+    return surface
+
+#def get_thumb(
 pygame.init()
 pygame.display.set_caption('Feed Me')
 
@@ -13,30 +33,23 @@ h=480
 size=(w,h)
 screen = pygame.display.set_mode(size)
 
+bg_colour = (255,255,255)
+screen.fill(bg_colour)
 
-def get_image(url):
-    
-    #Image displaying
-    img_file = urllib.request.urlopen(url)
-    img = BytesIO(img_file.read())
-    image = Image.open(img)
+url = "http://feeds.bbci.co.uk/news/rss.xml"
 
-    mode = image.mode
-    size = image.size
-    data = image.tostring()
+data = feedparser.parse(url)
 
-    assert mode in "RGB", "RGBA"
+#useful ones:
+#media_thumbnail, title, link, published, summary
+"""font = pygame.font.SysFont("comicsansms", 36)
+#text = font.render(data.entries[0].title, 1, (10, 10, 10))
+screen = screen.convert()
+screen.fill((250,250,250))
+text = font.render("Hello", 1, (10,10,10))
+textpos = text.get_rect()
 
-    #Convert image url to pygame surface
-    surface = pygame.image.fromstring(data, size, mode)
-    return surface
-
-
-
-#url = "http://feeds.bbci.co.uk/news/rss.xml"
-
-#data = feedparser.parse(url)
-
+screen.blit(text, textpos)"""
 
 #for datum in data:
 #    print(datum)
@@ -44,6 +57,16 @@ def get_image(url):
 #file = StringIO(x).read()
 
 
-img = get_image('http://i.imgur.com/CcjcXzj.jpg')
-screen.blit(img, (0,0))
+#img = get_image('http://i.imgur.com/CcjcXzj.jpg')
+#screen.blit(img, (0,0))
+font = pygame.font.SysFont("comicsansms", 36)
+text = font.render(data.entries[0]['title'], 1, (10,10,10))
+textpos = text.get_rect() #text height and width
+
+thumb = data.entries[0]['media_thumbnail']
+img = get_image(thumb[1]['url']) #1 gives bigger image. Could also use 0
+
+screen.blit(img,textpos)
+
+screen.blit(text, textpos)
 pygame.display.flip()
